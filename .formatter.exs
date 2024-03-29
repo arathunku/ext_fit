594819af1f027d09dbf607a42c63a5b43cef4cfe
Daniel Kukula <daniel.kuku@gmail.com>
2024-03-29 18:48:41 +0000

    Add credo and styler

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 # Used by "mix format"
 [
+  plugins: [Styler],
   inputs:
     ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
     |> Enum.flat_map(&Path.wildcard(&1, match_dot: true))
